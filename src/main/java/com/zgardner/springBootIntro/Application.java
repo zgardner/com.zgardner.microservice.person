9f4584db29a9e2eb0654f3da0b577f25ca285e7f
Zach Gardner <zgardner@keyholesoftware.com>
2016-06-03 20:43:50 -0500

    Use the configuration service to get the eureka server, and configure this to be an eureka client

--- src/main/java/com/zgardner/springBootIntro/Application.java
+++ src/main/java/com/zgardner/springBootIntro/Application.java
@@ -2,6 +2,7 @@ package com.zgardner.springBootIntro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableDiscoveryClient
 @EnableSwagger2
 public class Application 
 {
